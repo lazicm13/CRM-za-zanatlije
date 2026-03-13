10afea451ba16026becd72de4f47a065572ac3a6
lazicm13 <lazicm714@gmail.com>
2026-03-13 23:22:59 +0100

    CORS implemented

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.routers.voice import router as voice_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ili stavi tačan Lovable URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(voice_router)
